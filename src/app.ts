5dcd520fc383c6d58c096666e5e8bbd7559cee75
hafiz-ullah <mdhafizullah375.ff@gmail.com>
2024-04-17 04:58:41 +0300

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express();
-const port = 3000;
+const app: Application = express()
 
 app.use(cors())
 
@@ -10,7 +9,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('working successfully')
+  res.send('working successfully')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    try {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`database is connected successfully`)
 
-        await mongoose.connect(config.database_url as string);
-        console.log(`database is connected successfully`);
-
-        app.listen(config.port, () => {
-            console.log(`Application listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log(`failed to connect database`, err);
-    }
-
-
-
+    app.listen(config.port, () => {
+      console.log(`Application listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log(`failed to connect database`, err)
+  }
 }
-main();
+main()
